abed165157717b36526fe6d65cce4cd43d1ca17d
kingston <dkmurambadoro@gmail.com>
2025-10-16 06:28:53 -0400

    add specialization code

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,12 +1,11 @@
 const employees = [
-    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000},
-    {id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000},
-    {id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000},
-    {id: 4, name: 'Al Peterson', age: 60, department: 'HR', salary: 75000},
+    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript'},
+    {id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python'},
+    {id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java'},
+    {id: 4, name: 'Al Peterson', age: 60, department: 'HR', salary: 75000, specialization: 'SalesForce'},
 ];
 
 //Function to display all employees
-// Function to display all employees
 function displayEmployees() {
     const totalEmployees = employees
         .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`)
@@ -34,5 +33,15 @@ function findEmployeeById(employeeId) {
         document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     } else {
         document.getElementById('employeesDetails').innerHTML = 'No employee has been found with this ID';
+    }
+}
+
+// Function to find employees by specialization
+function searchBySpecialization(expertise) {
+    const foundEmployee = employees.find(employee => employee.specialization === expertise);
+    if(foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    } else {
+        document.getElementById('employeesDetails').innerHTML = 'No employee has been found with this specialization';
     }
 }

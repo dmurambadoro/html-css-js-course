364f4feff3f9c390ea9945f8cb4f51f4b6683a8e
kingston <dkmurambadoro@gmail.com>
2025-10-16 06:09:19 -0400

    add functions

---
+++ employeeDetails/employee_details.js
@@ -0,0 +1,38 @@
+const employees = [
+    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000},
+    {id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000},
+    {id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000},
+    {id: 4, name: 'Al Peterson', age: 60, department: 'HR', salary: 75000},
+];
+
+//Function to display all employees
+// Function to display all employees
+function displayEmployees() {
+    const totalEmployees = employees
+        .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`)
+        .join('');
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
+
+// Function to calculate total salaries
+function calculateTotalSalaries() {
+    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert(`Total salaries: $${totalSalaries}`)
+}
+
+// Function to display HR Employees
+function displayHREmployees() {
+    const hrEmployees = employees.filter(employee => employee.department === 'HR')
+    const hrEmployeesDisplay = hrEmployees.map((emp, index) => `<p>${emp.id}: ${emp.name}: ${emp.name} - ${emp.department} - $${emp.salary}`).join('');
+    document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
+}
+
+// Function to find employees by id
+function findEmployeeById(employeeId) {
+    const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if(foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    } else {
+        document.getElementById('employeesDetails').innerHTML = 'No employee has been found with this ID';
+    }
+}
